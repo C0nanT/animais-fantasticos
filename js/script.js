5dc738d6a70cd3b25bc6cb9e1100350c2c8ce5fd
Conan Torres <conantorreswf@gmail.com>
2023-08-28 18:15:23 -0300

    começando o modal

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import scrollToSection from "./scroll-suave.js";
 import scrollManual from "./scroll-manual.js";
 import listaAnimais from "./animais.js";
 import listaFaq from "./lista-faq.js";
+import modal from "./modal.js";
 
 scrollToSection();
 scrollManual();
 listaAnimais();
 listaFaq();
+modal();
