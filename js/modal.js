605dceac8e29a3cb0c73660ace1a20a46b249339
Conan Torres <conantorreswf@gmail.com>
2023-08-29 14:44:58 -0300

    modal

--- js/modal.js
+++ js/modal.js
@@ -1,1 +1,21 @@
-export default function modal() {}
+export default function modal() {
+  const abrirModal = document.querySelector('[data-modal="abrir-login"]');
+  const fecharModal = document.querySelector('[data-modal="fechar-login"]');
+  const containerModal = document.querySelector('[data-modal="container"]');
+
+  if (abrirModal && fecharModal && fecharModal) {
+    function toggleModal(event) {
+      event.preventDefault();
+      containerModal.classList.toggle("ativo");
+    }
+
+    function foraModal(event) {
+      if (event.target === this) {
+        toggleModal(event);
+      }
+    }
+    abrirModal.addEventListener("click", toggleModal);
+    fecharModal.addEventListener("click", toggleModal);
+    containerModal.addEventListener("click", foraModal);
+  }
+}
--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import scrollManual from "./scroll-manual.js";
 import listaAnimais from "./animais.js";
 import listaFaq from "./lista-faq.js";
 import modal from "./modal.js";
+import toolTip from "./tooltip.js";
 
 scrollToSection();
 scrollManual();
 listaAnimais();
 listaFaq();
 modal();
+toolTip();
